e667cbf3a8ef810b4611d00746e3575b638c0a24
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2014-02-09 11:39:08 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApprenticesApi::Application.config.secret_key_base = '617943f562b361b856c41625c0dea716f398e4a87239b76486dd37dce2f752f09917a5006d5aee175883fb9eb91c70ffa680935bbb69f25f28a5cf7f3e6214dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApprenticesApi::Application.config.session_store :cookie_store, key: '_apprentices-api_session'
